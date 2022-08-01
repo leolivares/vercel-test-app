a9480a1bbdedad5de1d43b821e48107cefce2e05
leolivares <leolivares@uc.cl>
2022-08-01 04:11:23 +0200

    🐛 Fix minor bug

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import List from './List';
 import TodoInput from './TodoInput';
 import './style.sass';
